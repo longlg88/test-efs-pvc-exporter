b76087361833467edb7324fde93b8a21b06a4280
longlg88 <longlg88@naver.com>
2019-10-01 17:15:47 +0900

    Test

--- temp.py
+++ temp.py
@@ -4,13 +4,11 @@ import sys
 from subprocess import Popen, PIPE, STDOUT
 import subprocess
 import timeit
-import time
 import json
 from datetime import datetime
 from threading import Thread
 
-# start = timeit.default_timer() # Record processing time
-start = time.time()
+start = timeit.default_timer() # Record processing time
 
 def human_bytes(B):
     """Return human readable file unit like KB, MB, GB string by Byte
@@ -103,8 +101,8 @@ def match_collect_info():
                     metric_list.append(metric_info)
     
     json_info = {"timestamp":str(datetime_now),"metadata":{ "pod":metric_list } } # Before change json type
-    print(json_info)
-    #return json_info    
+    #print(json_info)
+    return json_info    
 
 def all_efs_collect_info():
     """Return all efs directory volumes size
@@ -118,7 +116,6 @@ def all_efs_collect_info():
     datetime_now = datetime.now()
 
     for pv_name in pv_list:
-        print(pv_name)
         all_size_cmd = "kubectl exec -it "+get_efs_provisioner()+" -n kube-system -- du -ks /persistentvolumes/"+pv_name.replace('\n','')+ " | awk '{print $1}'"
         all_size_res = Popen(all_size_cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
         all_size = all_size_res.stdout.readlines()[1:]
@@ -129,95 +126,18 @@ def all_efs_collect_info():
         metric_list.append(metric_info)
 
     json_info = {"timestamp":str(datetime_now),"metadata":{ "pod":metric_list } } # Before change json type
-    print(json_info)
-    # return json_info
-
-# def collect_info():
-
-#     namespaces, pvc_names, pvc_ids = get_info() # namespaces, pvc_names, pvc_ids are list
-
-#     if not namespaces:
-#         print("Warning : Can't find namespaces \n"
-#             "You should check authorization in kubernetes cluster \n")
-#     else:
-#         get_efs_provisioner_name = Popen("kubectl get pod -n kube-system | grep efs | awk '{print $1}'", shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-#         efs_provisioner_name = get_efs_provisioner_name.stdout.read().replace('\n','')
-        
-#         metric_list = [] # Define metric list
-#         datetime_now = datetime.now() # Define current time
-
-#         """
-#         Collect size of pvc which mount each pods.
-#         List directories in 'persistentvolumes' directory and use 'du' command for collecting pvc size.
-#         """
-#         for val in range(len(namespaces)):
-#             find_dir_cmd = "kubectl exec -it " + efs_provisioner_name + " -n kube-system -- ls -al /persistentvolumes | awk '{print $9}' | grep " + pvc_names[val] + "-" + pvc_ids[val]
-
-#             try:
-#                 find_dir = Popen(find_dir_cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-#                 find_dir = find_dir.stdout.read()
-#             except subprocess.CalledProcessError as ex:
-#                 # output = ex.output
-#                 returncode = ex.returncode
-#                 if returncode != 1:
-#                     raise
-
-#             if find_dir:
-#                 pod_name_cmd = "kubectl describe pvc -n " + namespaces[val] + " " + pvc_names[val] + " | grep Mounted | awk '{print $3}'" # pod name
-#                 pod_name = Popen(pod_name_cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-#                 pod_name = pod_name.stdout.read()
-
-#                 if 'none' not in pod_name.replace('\n',''):
-#                     """
-#                     It has a bug --> du caculate too late some of kubernetes cluster and then tty timeout. So it doesn't calculate.
-#                     Using command for 'du' is like this.
-                    
-#                     cmd --> mount_size_cmd = "kubectl exec -it " + efs_provisioner_name + " -n kube-system -- du -c -hs /persistentvolumes/" + pvc_names[val] + "-" + pvc_ids[val] + " | awk '{print $1}'"
-#                     """
-                    
-#                     find_file_list_cmd = "kubectl exec -it " +efs_provisioner_name + " -n kube-system -- ls -al /persistentvolumes/" + pvc_names[val] + "-" + pvc_ids[val] + " | awk '{print $9}'" # find list
-#                     find_file_list = Popen(find_file_list_cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-#                     if find_file_list.stdout.readline() != "Unable to use a TTY - input is not a terminal or the right kind of file" or find_file_list.stdout.readline() != "." or find_file_list.stdout.readline() != ".." or "No such file or directory" not in find_file_list.stdout.readline():
-#                         find_file_list = find_file_list.stdout.readline()
-
-#                         if len(find_file_list) > 1:
-#                             mount_size=[]
-#                             for _file in find_file_list:
-#                                 m_size_cmd = "kubectl exec -it " + efs_provisioner_name + " -n kube-system -- du -ks /persistentvolumes/" + pvc_names[val] + "-" + pvc_ids[val] + "/" + _file + " | awk '{print $1}'"
-#                                 m_size = Popen(m_size_cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-#                                 m_size=m_size.stdout.read().split()
-#                                 mount_size.append(''.join(m_size))
-#                             mount_size = list(map(int, mount_size))
-                            
-#                             sum_size = sum(mount_size)
-#                             sum_size = human_bytes(sum_size*1024)
-                            
-#                             metric_info = {"namespace":namespaces[val], "name":pod_name.replace('\n',''), "size":str(sum_size), "pvc":pvc_names[val]}
-#                             metric_list.append(dict(metric_info))
-
-#                             #print("EFS PVC Monitor >> " + " namespace = " + namespaces[val] + "/ pod name = " + pod_name.replace('\n','') + "/ PVC size =  " + str(sum_size))
-#                         else:
-#                             metric_info = {"namespace":namespaces[val], "name":pod_name.replace('\n',''), "size":"4KB", "pvc":pvc_names[val]}
-#                             metric_list.append(dict(metric_info))
-
-#                         #print("EFS PVC Monitor >> " + " namespace = " + namespaces[val] + "/ pod name = " + pod_name.replace('\n','') +" / PVC size =  " + "4KB")
-
-#         json_info = {"timestamp":str(datetime_now),"metadata":{ "pod":metric_list } } # Before change json type
-#         print(json_info)
-#         #return json_info
+    # print(json_info)
+    return json_info
 
 if __name__ == "__main__":
-    
+    json_info = match_collect_info()
+    all_json_info = all_efs_collect_info()
+    #th_json_info=Thread(target=match_collect_info, args=())
+    #th_json_info.start()
+    #th_all_json_info=Thread(target=all_efs_collect_info, args=())
+    #th_all_json_info.start()
 
-    # json_info = match_collect_info()
-    # all_json_info = all_efs_collect_info()
-    th_json_info=Thread(target=match_collect_info, args=())
-    th_json_info.start()
-    th_all_json_info=Thread(target=all_efs_collect_info, args=())
-    th_all_json_info.start()
+    print(json.dumps(json_info))
+    print(json.dumps(all_json_info))
 
-    # print(json.dumps(json_info))
-    # print(json.dumps(all_json_info))
-
-# print('\nlaptime = ' % (timeit.default_timer() - start))
-print("\nlaptime = %d" % (time.time() - start))
+    print('\nlaptime = ' % (timeit.default_timer() - start))
